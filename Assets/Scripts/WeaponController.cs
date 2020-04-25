a19bf2117490c3ff18bc0ef92bd7c686c3ccb5e6
Ricardo Bustamante <ricardo@busta.dev>
2020-04-25 18:56:47 +0200

    Settle on game visuals and models.

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+   public Animator animator;
+   private static readonly int Attack1 = Animator.StringToHash("Attack");
+
+   public void Attack()
+   {
+      animator.SetTrigger(Attack1);
+   }
+}
